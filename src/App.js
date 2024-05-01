06c86c9b56934ff064581876822c88f3359c07e8
Abdul Rehman <abrehman4163@gmail.com>
2024-05-01 22:20:59 +0500

    Admin Dashboard and Patient pages completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Blogs from './Frontend/Pages/Blogs/Blogs';
-import Home from './Frontend/Pages/Home/Home';
-import Login from './Frontend/Pages/Login/Login';
+import Blogs from './Frontend/Pages/Blogs/Blogs.jsx';
+import Home from './Frontend/Pages/Home/Home.jsx';
+import Login from './Frontend/Pages/Login/Login.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
-import Dashboard from './Frontend/Pages/adminHome/Home';
-import Patient from './Frontend/Pages/adminPatients/Patient';
-import Package from './Frontend/Pages/adminPackage/Package';
+import Dashboard from './Frontend/Pages/adminHome/Home.jsx';
+import Patient from './Frontend/Pages/adminPatients/Patient.jsx';
+import Package from './Frontend/Pages/adminPackage/Package.jsx';
 function App() {
   return (
     <div className="app">
