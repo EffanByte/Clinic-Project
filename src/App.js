f4887cc87e20e833f6c2f54b1a714ff0309bdc72
Abdul Rehman <abrehman4163@gmail.com>
2024-05-01 21:35:42 +0500

    Admin Dashboard and Patient pages completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Blogs from './Frontend/Pages/Blogs/Blogs';
 import Home from './Frontend/Pages/Home/Home';
 import Login from './Frontend/Pages/Login/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import Dashboard from './Frontend/Pages/adminHome/Home';
+import Patient from './Frontend/Pages/adminPatients/Patient';
+import Package from './Frontend/Pages/adminPackage/Package';
 function App() {
   return (
-    <div>
+    <div className="app">
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Home/>}></Route>
     <Route path='/Blogs' element={<Blogs/>}></Route>
     <Route path='/Login' element={<Login/>}></Route>
+    <Route path='/Admin/Dashboard' element={<Dashboard/>}></Route>
+    <Route path='/Admin/Patient' element={<Patient/>}></Route>
+    <Route path='/Admin/Package' element={<Package/>}></Route>
     </Routes>
     </BrowserRouter>
     </div>
---
+++ src/Frontend/Pages/adminPackage/Package.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Package";
+
+function Package() {
+  return (
+    <div>
+      package
+    </div>
+  )
+}
+
+export default Package
