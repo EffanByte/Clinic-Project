bd0efd5e00450b0c2e265796f0764f09f2c391ce
EffanByte <94035974+EffanByte@users.noreply.github.com>
2024-05-01 13:58:19 +0500

    Added basic AWS image retrival, will add DB connection once tables created

---
+++ src/Backend/Models/Appointment.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+const { Schema } = mongoose;
+const appointmentSchema = new Schema({
+    // patient: { 
+    //     type: mongoose.Schema.Types.ObjectId, 
+    //     ref: 'Patient', 
+    //     required: true 
+    // },
+
+    patientCNIC: {
+        type: Number,
+        required: true
+    },
+    // doctor: { 
+    //     type: mongoose.Schema.Types.ObjectId, 
+    //     ref: 'Doctor', 
+    //     required: true 
+    // },
+    requestdate: {
+        type: date,
+        required: true
+    },
+    appointmentdate: {
+        type: date,
+        // required:true
+    },
+    // add in patientDetails image:{
+    //     type: String, 
+    // },
+    patientDetails: [{
+
+        weight: {
+            type: Number,
+            required: true
+        },
+        height: {
+            type: Number,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        bloodType: {
+            type: String,
+            required: true
+        },
+        temperature: {
+            type: Number,
+            required: true
+        },
+        symptoms: {
+            type: String,
+            required: true
+        },
+        bloodPressure:
+            [{
+                upper: {
+                    type: Number,
+                    required: true
+                },
+                lower: {
+                    type: Number,
+                    required: true
+                }
+            }],
+        Image: {
+            type: String,
+            required: true
+        }
+
+    }],
+    appointmentType: {
+        type: String,
+        enum: ["Follow Up", "New"],
+        required: true
+    },
+    appointmentStatus: {
+        type: String,
+        enum: ["Inprogress", "Completed", "Pending"],
+        required: true
+    }
+});
+const appointment = mongoose.model("Appointment", appointmentSchema);
+module.exports = appointment;
---
+++ src/Backend/Models/Payment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+// Define the schema for payments
+const paymentSchema = new mongoose.Schema({
+    // patient: { 
+    //     type: mongoose.Schema.Types.ObjectId, 
+    //     ref: 'Patient', 
+    //     required: true 
+    // },
+    patientCNIC: { 
+        type: Number,  
+        required: true 
+    },
+    amount: { 
+        type: Number, 
+        required: true 
+    },
+    date: { 
+        type: Date, 
+        default: Date.now 
+    },
+    method: { 
+        type: String, 
+        enum: ['Credit Card', 'Debit Card', 'PayPal', 'Cash'], 
+        required: true 
+    }
+});
+const Payment = mongoose.model('Payment', paymentSchema);
+module.exports = Payment;
