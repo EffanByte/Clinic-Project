fc354a978b78c5e37e985b32de641117242a3df4
Muhammad Ismaeel Zeb <109311287+ismaeel06@users.noreply.github.com>
2024-05-03 19:05:23 +0500

    Deleted History Table

--- src/Frontend/Pages/PatientHistory/PatientHistory.jsx
+++ src/Frontend/Pages/PatientHistory/PatientHistory.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import BlogsNav from '../../Components/Blogs/BlogsNav'
-import HistoryTable from '../../Components/PatientHistory/HistoryTable'
 
 function PatientHistory() {
   return (
     <div>
       <div style={{backgroundColor: "aliceblue"}}>
         <BlogsNav />
-        <HistoryTable />
       </div>
     </div>
   )
